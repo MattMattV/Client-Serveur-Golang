64b3ddefb509777e8b1ccc10eac038fc1e648bf4
Matthieu V <mvion@trace-software.com>
2015-11-13 10:12:07 +0100

    Etape 1 - Client Serveur simple

--- Client/client.go
+++ Client/client.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
 )
+
+func verifErreur(err error) {
+	if err != nil {
+		log.Panicf("Une erreur est survenue : \n\t%v", err)
+	}
+}
 
 func main() {
 
+	var message = "Coucou !"
+
+	fmt.Println("Lancement du client")
+
+	conn, err := net.Dial("tcp", "localhost:8080")
+	verifErreur(err)
+
+	fmt.Println("Connexion réussie")
+
+	encoder := gob.NewEncoder(conn)
+
+	err = encoder.Encode(message)
+	verifErreur(err)
+
+	fmt.Println("Le message à bien été envoyé.")
 }
--- Serveur/Serveur.go
+++ Serveur/Serveur.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
+	"log"
 	"net"
 )
 
+func verifErreur(err error) {
+	if err != nil {
+		log.Panicf("Une erreur est survenue : \n\t%v", err)
+	}
+}
+
 func handleConnection(conn net.Conn) {
 
+	var message string
+	var decoder = gob.NewDecoder(conn)
+
+	decoder.Decode(&message)
+	fmt.Printf("Le client dit : %s\n", message)
 }
 
 func main() {
 
-	
+	ln, err := net.Listen("tcp", ":8080")
+	verifErreur(err)
+	fmt.Println("Socket d'écoute créé")
+
+	for {
+		
+		conn, err := ln.Accept()
+		verifErreur(err)
+		
+		fmt.Println("Connexion détectée")
+
+		go handleConnection(conn)
+	}
 }
